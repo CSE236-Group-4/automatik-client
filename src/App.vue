277dfd50714447de0718d04bfe920a6bf5d04da9
elementalruler1 <a.wakeen@yahoo.com>
2020-11-20 11:18:21 -0500

    Created Register

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <b-container class="h-100">
-        <router-view v-on:auth="updateAuth" />
-    </b-container>
+    <router-view v-on:auth="updateAuth"/>
+  </b-container>
 </template>
 
 <script>
+// Add Firebase products here
+import 'firebase/auth';
+import 'firebase/firestore';
 import auth from './auth';
 
+const firebaseConfig = {
+  apiKey: 'API_KEY',
+  authDomain: 'PROJECT_ID.firebaseapp.com',
+  databaseURL: 'https://PROJECT_ID.firebaseio.com',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'PROJECT_ID.appspot.com',
+  messagingSenderId: 'SENDER_ID',
+  appId: 'APP_ID',
+  measurementId: 'G-MEASUREMENT_ID',
+};
+
 export default {
-    data() {
-        return {
-            loggedIn: auth.loggedIn(),
-        };
+  data() {
+    return {
+      loggedIn: auth.loggedIn(),
+    };
+  },
+  methods: {
+    updateAuth() {
+      this.loggedIn = auth.loggedIn();
     },
-    methods: {
-        updateAuth() {
-            this.loggedIn = auth.loggedIn();
-        },
-    },
+  },
 };
 </script>
 
