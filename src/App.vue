3a6ea6b26352df2f8d09d8dae079310c1f5878a5
elementalruler1 <a.wakeen@yahoo.com>
2020-11-20 11:18:21 -0500

    Created Register

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="h-100">
-        <router-view v-on:auth="updateAuth" />
-    </b-container>
+    <router-view v-on:auth="updateAuth"/>
+  </b-container>
 </template>
 
 <script>
--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 import Employees from './components/Employees.vue';
 import Schedules from './components/Schedules.vue';
+import Register from './components/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -49,6 +50,12 @@ const routes: Array<RouteConfig> = [
                 next('/login');
             }
         },
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+
     },
     {
         path: '*',
